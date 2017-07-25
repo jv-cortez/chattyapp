3995632c58cebcf828091ae7ff88eb5f96ad45a3
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-25 03:43:18 +0000

    added new html body

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx'
+import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+          <body>
+            <MessageList />
+            <ChatBar />
+          </body>
+      </div>
     );
   }
 }
