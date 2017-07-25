8de19613ae64249ec1bed400d7d3a132fccdc458
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-25 04:49:53 +0000

    fixed indentation

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div>
-            <MessageList />
-            <ChatBar />
+        <MessageList />
+        <ChatBar />
       </div>
     );
   }
