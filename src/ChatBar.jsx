5a63a942499f0b34d152ffc54a29cddfffef8216
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-25 23:58:34 +0000

    added text input functionality

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(){
+    super();
+    this.state = {
+      content:''
+    }
+    this.onContent = this.onContent.bind(this)
+    this.onPost = this.onPost.bind(this)
+  }
+
+  onContent(event) {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  onPost(keyPressed) {
+    const enterPressed = keyPressed.key
+    console.log("oP", enterPressed);
+    if (enterPressed === 'Enter') {
+      this.props.onNewPost(this.props.userName, this.state.content);
+    }
+  }
+
   render() {
     return (
        <div>  
         <footer className="chatbar">
-            <input className="chatbar-username" placeholder="Your Name (Optional)" />
-            <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+            <input
+              className="chatbar-username"
+              placeholder={this.props.userName}
+            />
+            <input 
+              className="chatbar-message" 
+              onInput={this.onContent}
+              onKeyUp={this.onPost}
+              placeholder="Type a message and hit ENTER"
+            />
         </footer>
       </div>
     )
