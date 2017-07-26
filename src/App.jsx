d6ff2ed5e5cea25e22064fd05678b584f3dcdeeb
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-26 00:01:20 +0000

    can generate message list from enter clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
-import MessageList from './MessageList.jsx'
-import ChatBar from './ChatBar.jsx'
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+import Data from '../data.json'; 
 
 class App extends Component {
+  
+  constructor() {
+    super()
+    this.state = Data
+    this.createMessage = this.createMessage.bind(this)
+  }
+
+
+  createMessage(username, content){
+    const message = {
+      username, content
+    }
+    const messageList = this.state.messages.concat(message)
+    console.log(messageList)
+    this.setState({
+        username: username,
+        messages: messageList
+      })
+  }
+
   render() {
     return (
       <div>
-        <MessageList />
-        <ChatBar />
+        <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList posts= {this.state.messages} />
+        <ChatBar 
+          userName={this.state.currentUser.name} 
+          onNewPost={this.createMessage} 
+        />
       </div>
     );
   }
 }
+
 export default App;
