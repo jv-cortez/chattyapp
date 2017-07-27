6c43f1aba4566ba5e74ce680b3eeb805eaa95d42
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-27 22:25:14 +0000

    added client count functionality

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -13,28 +13,42 @@ const server = express()
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
-
+//const clients = [];
+function updateUserCount(count) {
+  console.log('count', count)
+  const userUpdate = {
+    type: "updateUserCount",
+    count: count
+  }
+  wss.broadcast(userUpdate)
+}
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
+  //clients.push(ws)
   console.log('Client connected');
-
+  //after logging number of clients on App.jsx, add notifications everytime there is a new client connection " new user connected"
+  updateUserCount(wss.clients.size)
   ws.on('message', (message) => {
-    console.log('we want the funk', message)
     const incomingClientMessage = JSON.parse(message);
     incomingClientMessage.id = uuidv4();  
-    wss.broadcast(JSON.stringify(incomingClientMessage));
+    wss.broadcast(incomingClientMessage);
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    updateUserCount(wss.clients.size)
+  });
 });
 
 wss.broadcast = function broadcast(data) {
+  const stringified = JSON.stringify(data)
   wss.clients.forEach(function each(client) {
     if(client.readyState === WebSocket.OPEN) {
-      client.send(data);
+      client.send(stringified);
     }
   });
 };
+//broadcast current user count and when a client disconnects
