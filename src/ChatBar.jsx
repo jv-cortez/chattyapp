c181fe33554cfc4528f28d1f287d641e39b851ea
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-27 17:28:44 +0000

    able to send in username information and took out hardcoded username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,33 +4,45 @@ class ChatBar extends Component {
   constructor(){
     super();
     this.state = {
+      user:'',
       content:''
     }
     this.onContent = this.onContent.bind(this)
     this.onPost = this.onPost.bind(this)
+    this.onUserContent = this.onUserContent.bind(this)
+    this.onUserPost = this.onUserPost.bind(this)
   }
-
   onContent(event) {
     this.setState({
       content: event.target.value
     })
   }
-
   onPost(keyPressed) {
     const enterPressed = keyPressed.key
-    console.log("oP", enterPressed);
     if (enterPressed === 'Enter') {
-      this.props.onNewPost(this.props.userName, this.state.content);
+      this.props.onNewPost(this.state.user, this.state.content);
     }
   }
-
+  onUserContent(event) {
+    this.setState({
+      user: event.target.value,
+    })
+  }
+  onUserPost(keyPressed) {
+    const enterPressed = keyPressed.key;
+    if (enterPressed === 'Enter') {
+      this.props.onNewPost(this.state.user, this.state.content);
+    }
+  }
   render() {
     return (
        <div>  
         <footer className="chatbar">
             <input
               className="chatbar-username"
-              placeholder={this.props.userName}
+              onInput={this.onUserContent}
+              onKeyUp={this.onUserPost}
+              placeholder= "Username here"
             />
             <input 
               className="chatbar-message" 
