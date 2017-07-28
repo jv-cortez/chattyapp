fd7565b9622db6bb32b78242302bde2dc5403339
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-28 00:18:04 +0000

    added 'chatbot' to notify a new user or a user disconnecting

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ChatBar from './ChatBar.jsx';
 //add api??? emojis??? able to add imgs, figure out location
 // should scroll down when messages are at the end of the page
 //content should delete after pressing enter in the content input
-//set interval message that loads after 5 secs when the user connects 
 
 class App extends Component {
   
@@ -27,6 +26,13 @@ class App extends Component {
     this.ws.send(JSON.stringify(message)); //client message
   }
   componentDidMount() {
+    this.setState({
+      messages:[{
+        type:'1stMessage',
+        username: 'Bot',
+        content: 'Welcome to hell!'
+      }]
+    })
     this.ws = new WebSocket ('ws://localhost:3001');
     this.ws.onmessage = (event) => { 
       const message =JSON.parse(event.data)
@@ -40,8 +46,17 @@ class App extends Component {
           break;
         case 'updateUserCount':
           this.setState({
+            name: message.username,
+            content:message.content,
             usersConnected: message.count
           })
+          break;
+        case 'userDisconnected':
+        this.setState({
+          name: message.username,
+          content:message.content,
+          usersConnected: message.count
+        })
           break;
         default:
           throw new Error ('Unknown type' + message.type);
