091a76323e9e37227832d478e6e8d97a93ea41a8
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-29 00:02:50 +0000

    added chatty bot content when user entters the chatty app

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -5,19 +5,19 @@ const WebSocket = require('ws');
 
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('./'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new SocketServer({ server });
+
 function updateUserCount(count) {
   //Added console.log in order to see client count in terminal
   console.log('count', count)
   const userUpdate = {
     type: 'updateUserCount',
+    username: 'ChattyBot',
+    content: 'A Chatter entered the room!',
     count: count
   }
   wss.broadcast(userUpdate)
@@ -29,26 +29,22 @@ function updateUserCount(count) {
   const userUpdate = {
     type: 'userDisconnected',
     username: 'ChattyBot',
-    content: 'A User left!',
+    content: 'A Chatter left!',
     count: count
   }
   wss.broadcast(userUpdate)
 }
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
   
-  //after logging number of clients on App.jsx, add notifications everytime there is a new client connection " new user connected"
   updateUserCount(wss.clients.size)
+  
   ws.on('message', (message) => {
     const incomingClientMessage = JSON.parse(message);
     incomingClientMessage.id = uuidv4();  
     wss.broadcast(incomingClientMessage);
   });
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log('Client disconnected')
     userDisconnected(wss.clients.size)
