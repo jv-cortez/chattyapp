75c07e5bf9b61ed0495d4a642fbf0a7e357dcbaa
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-02 01:24:03 +0000

    added comments for each function to clarify their usage

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -14,6 +14,7 @@ class ChatBar extends Component {
     this.onUserContent = this.onUserContent.bind(this);
     this.onUserPost = this.onUserPost.bind(this);
   }
+  //function to handle user's chosen username text color
   handleColorChange(event){
     this.setState({
       color: event.target.value
@@ -21,11 +22,13 @@ class ChatBar extends Component {
       this.props.onNewPost('usernameTextColor', this.state.user,'',this.state.color);
     });
   }
+  //function to handle input from chat message or content
   onContent(event) {
     this.setState({
       content: event.target.value
     });
   }
+  //user,content, and color choice are sent to client-side App.jsx file when enter is pressed 
   onPost(keyPressed) {
     const enterPressed = keyPressed.key;
     if (enterPressed === 'Enter') {
@@ -33,14 +36,17 @@ class ChatBar extends Component {
       this.setState({
         content:''
       });
+      //when a message is sent and is already at the bottom of the window, this code will scroll the window down automatically 
       window.scrollTo(0,document.body.scrollHeight);
     }
   }
+  //sets inputted username to state
   onUserContent(event) {
     this.setState({
-      user: event.target.value,
+      user: event.target.value
     });
   }
+  //sends username text and color to client-side App.jsx when enter is pressed
   onUserPost(keyPressed) {
     const enterPressed = keyPressed.key;
     if (enterPressed === 'Enter') {
