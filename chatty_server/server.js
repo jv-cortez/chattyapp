787ea142ef1aa634e78b813e29341b5827a4aa02
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-02 01:52:31 +0000

    added semicolons, fixed spaces, and added comments for clarity

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
-const uuidv4 = require('uuid/v4')
+const uuidv4 = require('uuid/v4');
 const WebSocket = require('ws');
 
 const PORT = 3001;
@@ -11,33 +11,34 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
+//updates user count when client connects to the server
 function updateUserCount(count) {
   //Added console.log in order to see client count in terminal
-  console.log('count', count)
+  console.log('count', count);
   const userUpdate = {
     type: 'updateUserCount',
     username: 'ChattyBot',
     content: 'A Chatter entered the room!',
     count: count
-  }
-  wss.broadcast(userUpdate)
+  };
+  wss.broadcast(userUpdate);
 }
-
- function userDisconnected(count) {
+//updates user count when user disconnects from server
+function userDisconnected(count) {
   //Added console.log in order to see client count in terminal
-  console.log('count', count)
+  console.log('count', count);
   const userUpdate = {
     type: 'userDisconnected',
     username: 'ChattyBot',
     content: 'A Chatter left!',
     count: count
   }
-  wss.broadcast(userUpdate)
+  wss.broadcast(userUpdate);
 }
 wss.on('connection', (ws) => {
   console.log('Client connected');
   
-  updateUserCount(wss.clients.size)
+  updateUserCount(wss.clients.size);
   
   ws.on('message', (message) => {
     const incomingClientMessage = JSON.parse(message);
@@ -46,13 +47,13 @@ wss.on('connection', (ws) => {
   });
 
   ws.on('close', () => {
-    console.log('Client disconnected')
-    userDisconnected(wss.clients.size)
+    console.log('Client disconnected');
+    userDisconnected(wss.clients.size);
   });
 });
 
 wss.broadcast = function broadcast(data) {
-  const stringified = JSON.stringify(data)
+  const stringified = JSON.stringify(data);
   wss.clients.forEach(function each(client) {
     if(client.readyState === WebSocket.OPEN) {
       client.send(stringified);
